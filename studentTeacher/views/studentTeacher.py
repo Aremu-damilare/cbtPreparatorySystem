5cdaecd3eb7bb14ff031b9050a77e0e4de18a832
aremu-damilare <a.matthew2013@gmail.com>
2021-02-10 12:34:52 +0100

    first commit

---
+++ studentTeacher/views/studentTeacher.py
@@ -0,0 +1,23 @@
+from django.shortcuts import redirect, render
+from django.views.generic import TemplateView
+from django.db.models import Count
+from studentTeacher.models import Quiz
+
+class SignUpView(TemplateView):
+    template_name = 'registration/signup.html'
+
+def home(request):
+    if request.user.is_authenticated:
+        if request.user.is_teacher:
+            return redirect('teachers:quiz_change_list')
+        elif request.user.is_student:
+            return redirect('students:quiz_list')
+        else:
+            return redirect('admin:index')
+
+    return render(request,'studentTeacher/quiz_list.html',{
+        'quizzes':Quiz.objects.annotate(questions_count=Count('questions')) \
+            .filter(questions_count__gt=0)})
+
+class AboutView(TemplateView):
+    template_name = 'studentTeacher/about.html'
